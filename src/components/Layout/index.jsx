206bb4ab07d51214a3c450a2e6f7df3879a2cd22
madhavpatel <madhav.patel@differenzsystem.com>
2024-01-16 10:32:26 +0530

    Vite Config commit on 16-01-2024

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -28,7 +28,7 @@ const Layout = () => {
   }, []);
 
   return !loading ? (
-    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400 blog_card">
       <div className="w-full block">
         <Header />
         <main>
